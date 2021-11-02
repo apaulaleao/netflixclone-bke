78515620eb3447db95f70fe03cebd15d1f5339f5
apaulaleao <paula.leao@gmail.com>
2021-11-01 22:49:01 -0300

    find

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+import { Controller, Body, Post } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { UserRole } from './enum/role.enum';
+
+@Controller()
+export class UsersController {
+  constructor(private service: UsersService) {}
+
+  @Post('create-user')
+  createUser(@Body() data: CreateUserDto): Promise<User> {
+    delete data.passwordConfirmation;
+    return this.service.create(data, UserRole.USER);
+  }
+
+  @Post('create-admin')
+  createAdmin(@Body() data: CreateUserDto): Promise<User> {
+    delete data.passwordConfirmation;
+    return this.service.create(data, UserRole.ADMIN);
+  }
+@Get('find/:id')
+findOne(@Param('id') id:string): Promise<User>{
+    return this.service.findOne(id);
+}
+
+@Get('findAll/:id')
+find(@Param('id') id:string): Promise<User>{
+    return this.service.findAll(id);
+
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { UserRole } from './enum/role.enum';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {}
